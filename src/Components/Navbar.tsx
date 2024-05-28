c8d726c76e930c5e1fb48c82063f0fd85d41f602
ttgannon <ga1179@nyu.edu>
2024-05-28 12:53:35 -0400

    resonsive navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,16 +1,28 @@
-import { Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const NavBar = () => {
   return (
-    <Navbar data-bs-theme="dark" className="bg-customRed flex justify-center">
-      <Nav>
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/artiststatement">Artist Statement</Nav.Link>
-        <Nav.Link href="/gallery">Gallery</Nav.Link>
-        <Nav.Link href="/exhibitions">Exhibitions</Nav.Link>
-        <Nav.Link href="/contact">Contact</Nav.Link>
-        <Nav.Link href="/shop">Shop</Nav.Link>
-      </Nav>
+    <Navbar
+      expand="sm"
+      data-bs-theme="dark"
+      className="bg-customRed flex items-center justify-center"
+    >
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="flex justify-center items-center"
+        >
+          <Nav>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/artiststatement">Artist Statement</Nav.Link>
+            <Nav.Link href="/gallery">Gallery</Nav.Link>
+            <Nav.Link href="/exhibitions">Exhibitions</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/shop">Shop</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };

e3ce4656c266bf54c98ec408f13cf9be2ec0c771
ttgannon <ga1179@nyu.edu>
2024-06-19 17:34:57 -0300

    shop page

--- src/Components/Shop.tsx
+++ src/Components/Shop.tsx
@@ -1,3 +1,22 @@
+import { Button } from "react-bootstrap";
+
+function handleClick() {
+  window.location.href =
+    "https://fineartamerica.com/profiles/maureen-squires/shop";
+}
+
 export const Shop = () => {
-  return "helo";
+  return (
+    <>
+      <div className="flex items-center justify-center m-6">
+        <h1 className="text-center text-4xl max-w-2xl">
+          Take these styles with you everywhere: custom mugs, tote bags, and
+          t-shirts with your favorite designs.
+        </h1>
+      </div>
+      <div className="items-center flex justify-center">
+        <Button onClick={handleClick}>Shop Now</Button>
+      </div>
+    </>
+  );
 };

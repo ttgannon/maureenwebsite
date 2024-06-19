5416724b32e171584ed6b6eb9f549db982fbe3b6
ttgannon <ga1179@nyu.edu>
2024-06-19 17:35:08 -0300

    artist statement page

---
+++ src/Components/Statement.tsx
@@ -0,0 +1,18 @@
+export default function Statement() {
+  return (
+    <div className="text-center justify-center">
+      <h1 className="text-9xl">Maureen Squires</h1>
+      <h2>
+        Maureen Squires is a painter/calligrapher who approaches lettering from
+        a painter's perspective and exhibits her work widely. She majored in
+        painting at Seton Hill University and afterwards studied advanced
+        calligraphy at Carnegie Melon with Arnold Banks and other fine
+        calligraphers. She enjoys interpreting the words of others or simply
+        experimenting with the abstract forms of the alphabet but also makes
+        forays into landscape and abstract painting. Her tools range from
+        traditional steel nibs to bamboo--"anything that writes"--using ink,
+        acrylic, gouache, and Japanese watercolors
+      </h2>
+    </div>
+  );
+}

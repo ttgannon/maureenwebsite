67478c78310d4e83470b28bed50300c551431550
ttgannon <ga1179@nyu.edu>
2024-06-19 17:34:41 -0300

    exhibitions page

---
+++ src/Components/Exhibitions.tsx
@@ -0,0 +1,10 @@
+import { Button } from "react-bootstrap";
+
+export default function Exhibitions() {
+  return (
+    <div className="text-center relative flex justify-center">
+      <h1 className="text-9xl absolute">Exhibitions</h1>
+      <img src="public/gallery.jpeg"></img>
+    </div>
+  );
+}
